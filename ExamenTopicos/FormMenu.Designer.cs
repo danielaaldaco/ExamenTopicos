ed594a3bd32a24d60f94f70c3c3edbe67546e451
danielaaldaco <danielaaldaco9@gmail.com>
2024-11-18 14:34:47 -0600

    Se termino la vista del menu para el usuario y se nombraron los botones correctamente

--- ExamenTopicos/FormMenu.Designer.cs
+++ ExamenTopicos/FormMenu.Designer.cs
@@ -29,15 +29,24 @@
         private void InitializeComponent()
         {
             btnPuestos = new Button();
-            button2 = new Button();
+            btnEditoriales = new Button();
+            btnDescuentos = new Button();
+            btnTiendas = new Button();
+            btnVentas = new Button();
+            btnEmpleados = new Button();
+            btnTitulos = new Button();
+            btnRegalias = new Button();
+            btnAutores = new Button();
+            btnInfoEditorial = new Button();
+            btnAutoresLibros = new Button();
             SuspendLayout();
             // 
             // btnPuestos
             // 
             btnPuestos.Image = Properties.Resources.icons8_work_50;
-            btnPuestos.Location = new Point(46, 54);
+            btnPuestos.Location = new Point(46, 28);
             btnPuestos.Name = "btnPuestos";
-            btnPuestos.Size = new Size(201, 127);
+            btnPuestos.Size = new Size(201, 186);
             btnPuestos.TabIndex = 0;
             btnPuestos.Text = "Puestos";
             btnPuestos.TextAlign = ContentAlignment.BottomCenter;
@@ -45,23 +54,139 @@
             btnPuestos.UseVisualStyleBackColor = true;
             btnPuestos.Click += btnPuestos_Click;
             // 
-            // button2
+            // btnEditoriales
             // 
-            button2.Image = Properties.Resources.icons8_books_50;
-            button2.Location = new Point(46, 196);
-            button2.Name = "button2";
-            button2.Size = new Size(201, 85);
-            button2.TabIndex = 1;
-            button2.Text = "Editoriales";
-            button2.TextImageRelation = TextImageRelation.TextBeforeImage;
-            button2.UseVisualStyleBackColor = true;
+            btnEditoriales.Image = Properties.Resources.icons8_books_50;
+            btnEditoriales.Location = new Point(46, 229);
+            btnEditoriales.Name = "btnEditoriales";
+            btnEditoriales.Size = new Size(201, 85);
+            btnEditoriales.TabIndex = 1;
+            btnEditoriales.Text = "Editoriales";
+            btnEditoriales.TextImageRelation = TextImageRelation.TextBeforeImage;
+            btnEditoriales.UseVisualStyleBackColor = true;
+            // 
+            // btnDescuentos
+            // 
+            btnDescuentos.Image = Properties.Resources.icons8_discount_50;
+            btnDescuentos.Location = new Point(46, 333);
+            btnDescuentos.Name = "btnDescuentos";
+            btnDescuentos.Size = new Size(201, 85);
+            btnDescuentos.TabIndex = 2;
+            btnDescuentos.Text = "Descuentos";
+            btnDescuentos.TextImageRelation = TextImageRelation.TextBeforeImage;
+            btnDescuentos.UseVisualStyleBackColor = true;
+            // 
+            // btnTiendas
+            // 
+            btnTiendas.Image = Properties.Resources.icons8_store_50;
+            btnTiendas.Location = new Point(272, 28);
+            btnTiendas.Name = "btnTiendas";
+            btnTiendas.Size = new Size(99, 85);
+            btnTiendas.TabIndex = 3;
+            btnTiendas.Text = "Tiendas";
+            btnTiendas.TextAlign = ContentAlignment.BottomCenter;
+            btnTiendas.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnTiendas.UseVisualStyleBackColor = true;
+            // 
+            // btnVentas
+            // 
+            btnVentas.Image = Properties.Resources.icons8_sales_48;
+            btnVentas.Location = new Point(377, 28);
+            btnVentas.Name = "btnVentas";
+            btnVentas.Size = new Size(99, 85);
+            btnVentas.TabIndex = 4;
+            btnVentas.Text = "Ventas";
+            btnVentas.TextAlign = ContentAlignment.BottomCenter;
+            btnVentas.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnVentas.UseVisualStyleBackColor = true;
+            // 
+            // btnEmpleados
+            // 
+            btnEmpleados.Image = Properties.Resources.icons8_management_50;
+            btnEmpleados.Location = new Point(272, 129);
+            btnEmpleados.Name = "btnEmpleados";
+            btnEmpleados.Size = new Size(201, 85);
+            btnEmpleados.TabIndex = 5;
+            btnEmpleados.Text = "Empleados";
+            btnEmpleados.TextAlign = ContentAlignment.MiddleRight;
+            btnEmpleados.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnEmpleados.UseVisualStyleBackColor = true;
+            // 
+            // btnTitulos
+            // 
+            btnTitulos.Image = Properties.Resources.icons8_editor_64;
+            btnTitulos.Location = new Point(272, 232);
+            btnTitulos.Name = "btnTitulos";
+            btnTitulos.Size = new Size(201, 186);
+            btnTitulos.TabIndex = 6;
+            btnTitulos.Text = "Titulos";
+            btnTitulos.TextAlign = ContentAlignment.BottomCenter;
+            btnTitulos.TextImageRelation = TextImageRelation.TextAboveImage;
+            btnTitulos.UseVisualStyleBackColor = true;
+            // 
+            // btnRegalias
+            // 
+            btnRegalias.Image = Properties.Resources.icons8_cash_60;
+            btnRegalias.Location = new Point(496, 28);
+            btnRegalias.Name = "btnRegalias";
+            btnRegalias.Size = new Size(102, 186);
+            btnRegalias.TabIndex = 7;
+            btnRegalias.Text = "Regalias";
+            btnRegalias.TextAlign = ContentAlignment.BottomCenter;
+            btnRegalias.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnRegalias.UseVisualStyleBackColor = true;
+            // 
+            // btnAutores
+            // 
+            btnAutores.Image = Properties.Resources.icons8_book_and_pencil_48;
+            btnAutores.Location = new Point(614, 28);
+            btnAutores.Name = "btnAutores";
+            btnAutores.Size = new Size(102, 186);
+            btnAutores.TabIndex = 8;
+            btnAutores.Text = "Autores";
+            btnAutores.TextAlign = ContentAlignment.BottomCenter;
+            btnAutores.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnAutores.UseVisualStyleBackColor = true;
+            // 
+            // btnInfoEditorial
+            // 
+            btnInfoEditorial.Image = Properties.Resources.icons8_open_book_60;
+            btnInfoEditorial.Location = new Point(614, 232);
+            btnInfoEditorial.Name = "btnInfoEditorial";
+            btnInfoEditorial.Size = new Size(102, 186);
+            btnInfoEditorial.TabIndex = 10;
+            btnInfoEditorial.Text = "Detalle de editorial";
+            btnInfoEditorial.TextAlign = ContentAlignment.BottomCenter;
+            btnInfoEditorial.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnInfoEditorial.UseVisualStyleBackColor = true;
+            // 
+            // btnAutoresLibros
+            // 
+            btnAutoresLibros.Image = Properties.Resources.icons8_document_writer_50;
+            btnAutoresLibros.Location = new Point(496, 232);
+            btnAutoresLibros.Name = "btnAutoresLibros";
+            btnAutoresLibros.Size = new Size(102, 186);
+            btnAutoresLibros.TabIndex = 9;
+            btnAutoresLibros.Text = "Autores-Libros";
+            btnAutoresLibros.TextAlign = ContentAlignment.BottomCenter;
+            btnAutoresLibros.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnAutoresLibros.UseVisualStyleBackColor = true;
             // 
             // FormMenu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button2);
+            ClientSize = new Size(774, 450);
+            Controls.Add(btnInfoEditorial);
+            Controls.Add(btnAutoresLibros);
+            Controls.Add(btnAutores);
+            Controls.Add(btnRegalias);
+            Controls.Add(btnTitulos);
+            Controls.Add(btnEmpleados);
+            Controls.Add(btnVentas);
+            Controls.Add(btnTiendas);
+            Controls.Add(btnDescuentos);
+            Controls.Add(btnEditoriales);
             Controls.Add(btnPuestos);
             Name = "FormMenu";
             Text = "FormMenu";
@@ -71,6 +196,15 @@
         #endregion
 
         private Button btnPuestos;
-        private Button button2;
+        private Button btnEditoriales;
+        private Button btnDescuentos;
+        private Button btnTiendas;
+        private Button btnVentas;
+        private Button btnEmpleados;
+        private Button btnTitulos;
+        private Button btnRegalias;
+        private Button btnAutores;
+        private Button btnInfoEditorial;
+        private Button btnAutoresLibros;
     }
 }
